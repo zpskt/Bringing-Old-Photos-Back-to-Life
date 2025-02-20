f0996b321e163fc79fcb385997e616d9987cf8c1
zpskt <2233151897@qq.com>
2025-02-20 17:51:27 +0800

    添加Django服务

---
+++ backend/repaired_old_photo/repaired_old_photo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for repaired_old_photo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'repaired_old_photo.settings')
+
+application = get_asgi_application()
